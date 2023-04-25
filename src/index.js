9236e4d090009577c30c41bd4467813ebe44f7b6
Eisuke Esaki <eisukeesaki@gmail.com>
2023-04-24 19:59:01 -0700

    added exit condition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const ChangesStream = require('changes-stream');
+const request = require('rock-req');
 
 const db = 'https://replicate.npmjs.com';
 
 var changes = new ChangesStream({
   db: db,
+  include_docs: true,
 });
 
-changes.on('data', (change) => {
-  console.log('change: %o', change);
+request.get(db, (err, res, data) => { // data is type of Buffer
+  const body = JSON.parse(data.toString());
+  const end_sequence = body.end_seq;
+
+  changes.on('data', (change) => {
+    if (change.seq >= end_sequence)
+      process.exit(0);
+    console.log(change.doc);
+  });
 });
+
